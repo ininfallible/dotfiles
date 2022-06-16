e169036a063da46fa61923d947523a99a5983ce6
Derek Jiang <derek.l.jiang@gmail.com>
2022-06-16 14:24:05 -0700

    rebound telescope commands

--- nvim/init.lua
+++ nvim/init.lua
@@ -144,15 +144,15 @@ require('telescope').load_extension 'fzf'
 
 --Add leader shortcuts
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
-vim.keymap.set('n', '<leader>sf', function()
+vim.keymap.set('n', '<leader>ff', function()
   require('telescope.builtin').find_files { previewer = false }
 end)
-vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find)
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags)
-vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string)
-vim.keymap.set('n', '<leader>sp', require('telescope.builtin').live_grep)
-vim.keymap.set('n', '<leader>so', function()
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').current_buffer_fuzzy_find)
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
+vim.keymap.set('n', '<leader>ft', require('telescope.builtin').tags)
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').grep_string)
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
+vim.keymap.set('n', '<leader>fo', function()
   require('telescope.builtin').tags { only_current_buffer = true }
 end)
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles)
